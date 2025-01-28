ea1df7a3a89258bac80774e1057800991cdbddda
aurora9721 <55090088+auroranv@users.noreply.github.com>
2025-01-28 16:50:31 +0200

    .

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-jQuery(function($) {
-  $(window).scroll(function() {
-      var scrollTop = $(this).scrollTop();
+jQuery(function ($) {
+  $(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
 
-          $('header').css({
-          opacity: function() {
-              var elementHeight = $(this).height(),
-              opacity = ((elementHeight - scrollTop) / elementHeight);
-              return opacity;
-          }
-      });
+    $("header").css({
+      opacity: function () {
+        var elementHeight = $(this).height(),
+          opacity = (elementHeight - scrollTop) / elementHeight;
+        return opacity;
+      },
+    });
   });
-})
-
+});
 
 // jQuery(function($) {
 // var divs = $('.name'),
